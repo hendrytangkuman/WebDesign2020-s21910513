566cfac165f44a882a7ea361298fed0367ab64d9
unknown <hendrytangkuman2@gmail.com>
2020-11-11 12:39:15 +0800

    exercise

--- pertemuan 20/script.js
+++ pertemuan 20/script.js
@@ -1,1 +1,70 @@
-console.log("Halo semua!");
+// variable
+
+// let & const
+
+// let umur= 25;
+// umur= 26;
+// let nilai=95.5;
+// let sudahMenikah= true;
+// let nama="johnDoe";
+// console.log(umur);
+// console.log(nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir= 2001;
+// console.log(tahunLahir);
+
+// operator
+
+// arithmetic operator
+
+// let angka1= 10;
+// let angka2= 20;
+
+// console.log(angka1+angka2);
+
+// let umur=18;
+// let namaDepan="hendry";
+// let namaBelakang="tangkuman";
+
+// console.log("hello! my name is "+ namaDepan + " " + namaBelakang + ", i'm " + umur + " y.o");
+
+// boolean perbandingan
+
+// let angka1= 10;
+// let angka2= 9;
+
+// console.log(angka1>=angka2);
+
+// ternary operator
+
+// let angka= 5;
+// let hasil= angka <=0 ? "negatif" : "positif";
+
+// console.log(hasil);
+
+// // operator logika
+
+// let statement1= 1<2;
+// let statement2= true;
+
+// console.log(statement1 && statement2);
+// console.log(statement1);
+
+// let angka= prompt("masukkan angka= ");
+// console.log("angka yang dimasukkan adalah= " + angka);
+
+// Exercise
+
+let bmi;
+let berat= prompt("input berat= ");
+let tinggi= prompt("input tinggi= ");
+
+bmi= berat / (tinggi * tinggi);
+console.log("berat= " + berat);
+console.log("tinggi= " + tinggi);
+console.log("Body mass index= " + bmi);
+
+
